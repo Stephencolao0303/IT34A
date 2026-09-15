8f8d1cf289113761890050eddba511796f703c4e
hansel <johnhanselamores@gmail.com>
2026-09-15 14:23:52 +0800

    added activity logs admin

--- app/auth/signout.php
+++ app/auth/signout.php
@@ -1,11 +1,24 @@
-<?php 
-require_once '../../config/functions.php';
+<?php
+
+require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../../config/functions.php';
+
+if (isset($_SESSION['user_id'])) {
+
+    logActivity(
+        $pdo,
+        $_SESSION['user_id'],
+        $_SESSION['user_email'],
+        'logout',
+        'success'
+    );
+}
 
 $_SESSION = [];
 
 session_destroy();
 
-header("Location: " . BASE_URL . "/index.php");
+header("Location: " . BASE_URL . "/test/index.php");
 
 exit;
 
